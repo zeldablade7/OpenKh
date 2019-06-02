87dada175b38d52bd561a70582411720f49249f2
Rikux3 <barack030@web.de>
2019-06-02 20:01:35 +0200

    Big endian BinaryReader for PSARC/PS3 Asset files

---
+++ OpenKh.Common/BigEndianBinaryReader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace OpenKh.Common
+{
+    public class BigEndianBinaryReader : BinaryReader
+    {
+        public BigEndianBinaryReader(Stream stream) : base(stream) { }
+
+        public override short ReadInt16()
+        {
+            var data = base.ReadBytes(2);
+            Array.Reverse(data);
+            return BitConverter.ToInt16(data, 0);
+        }
+
+        public override int ReadInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        public override long ReadInt64()
+        {
+            var data = base.ReadBytes(8);
+            Array.Reverse(data);
+            return BitConverter.ToInt64(data, 0);
+        }
+
+        public override ushort ReadUInt16()
+        {
+            var data = base.ReadBytes(2);
+            Array.Reverse(data);
+            return BitConverter.ToUInt16(data, 0);
+        }
+
+        public override uint ReadUInt32()
+        {
+            var data = base.ReadBytes(4);
+            Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
+        }
+
+        public override ulong ReadUInt64()
+        {
+            var data = base.ReadBytes(8);
+            Array.Reverse(data);
+            return BitConverter.ToUInt64(data, 0);
+        }
+
+        public ulong ReadUInt40()
+        {
+            var data = new byte[8];
+            base.ReadBytes(5).CopyTo(data, 3);
+            Array.Reverse(data);
+            return BitConverter.ToUInt64(data, 0);
+        }
+    }
+}
