872f500705d41ed048447ab8630c860f2ff44eae
Rikux3 <barack030@web.de>
2019-06-03 20:18:43 +0200

    Add tests for Psarc

---
+++ OpenKh.Tests/Archives/PsarcTests.cs
@@ -0,0 +1,22 @@
+﻿using OpenKh.Common.Archives;
+using Xunit;
+
+namespace OpenKh.Tests.Archives
+{
+    public class PsarcTests
+    {
+        [Fact]
+        public void IsAmountOfTocEntriesCorrect() => Helpers.UseAsset("test.psarc", stream =>
+        {
+            Psarc psarc = new Psarc(stream);
+            Assert.Equal(3, psarc.Toc.Count);
+        });
+
+        [Fact]
+        public void HasExpectedFile() => Helpers.UseAsset("test.psarc", stream =>
+        {
+            Psarc psarc = new Psarc(stream);
+            Assert.True((psarc?.Toc[2]?.FileName ?? string.Empty) == "somefolder/somefile.bin");
+        });
+    }
+}
