139e55bdff4014a6b86832358f6ad75ea11713e1
Rikux3 <d.drankov@gmail.com>
2019-12-23 18:37:36 +0100

    minor refactorings

---
+++ OpenKh.Audio/IAudioContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OpenKh.Audio
+{
+    public interface IAudioContainer
+    {
+        IEnumerable<IAudio> Entries { get; }
+    }
+}
--- OpenKh.Audio/Vag.cs
+++ OpenKh.Audio/Vag.cs
@@ -1,8 +1,8 @@
 ﻿//https://github.com/ColdSauce/psxsdk/blob/master/tools/vag2wav.c
 //https://github.com/losnoco/vgmstream/blob/master/src/meta/vag.c
 //https://gitlab.com/kenjiuno/khkh_xldM/blob/master/khiiMapv/ParseSD.cs
-using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Xe.IO;
 
 namespace OpenKh.Audio
@@ -68,44 +68,24 @@ namespace OpenKh.Audio
             else
                 Channels = 1;
 
-            reader.BaseStream.Position = 48 + 16 * Channels;
+            reader.BaseStream.Position = 0x20;
+            StreamName = Encoding.UTF8.GetString(reader.ReadBytes(0x10)).Replace("\0", string.Empty);
 
+            reader.BaseStream.Position = 48 + 16 * Channels;
             wav = Decode(stream);
         }
 
         private Stream Decode(Stream stream)
         {
-            var bytelist = Channels == 2 ? Decode2Channel(stream) : Decode1Channel(stream);
+            var wavstream = Channels == 2 ? GetTwoChannelWavStream(stream) : GetOneChannelWavStream(stream);
+            var wavheader = AudioHelpers.BuildWavHeader((int)wavstream.Length, Channels, SampleRate);
+            wavheader.Position = wavheader.Length;
+            wavstream.CopyTo(wavheader);
 
-            var ms = AudioHelpers.BuildWavHeader((int)bytelist.Length, Channels, SampleRate);
-            //BinaryWriter writer = new BinaryWriter(ms);
-            //bytelist.ForEach(d => writer.Write(d));
-
-            ms.Position = ms.Length;
-            bytelist.CopyTo(ms);
-
-            return ms;
+            return wavheader;
         }
 
-        private double DecodeBlockA(int num6, double prevAdd, double inout, int shift_factor, int predict_nr)
-        {
-            int d = (num6 & 15) << 12;
-            if ((d & 32768) != 0)
-                d |= -65536;
-            inout = (d >> shift_factor) + prevAdd * f[predict_nr, 0] + inout * f[predict_nr, 1];
-            return inout;
-        }
-
-        private double DecodeBlockB(int num6, double prevAdd, double inout, int shift_factor, int predict_nr)
-        {
-            int d = (num6 & 240) << 8;
-            if ((d & 32768) != 0)
-                d |= -65536;
-            inout = (d >> shift_factor) + prevAdd * f[predict_nr, 0] + inout * f[predict_nr, 1];
-            return inout;
-        }
-
-        private Stream Decode1Channel(Stream stream)
+        private Stream GetOneChannelWavStream(Stream stream)
         {
             MemoryStream outStream = new MemoryStream();
             BinaryWriter writer = new BinaryWriter(outStream);
@@ -125,20 +105,20 @@ namespace OpenKh.Audio
                     int shift_factor = predict_nr & 15;
                     predict_nr >>= 4;
 
-                    for (int i = 2; i < 16; i++)
+                    for (int i = 2; i < 16; ++i)
                     {
                         int num6 = buffer[i];
-                        num2 = DecodeBlockA(num6, num1, num2, shift_factor, predict_nr);
+                        num2 = DecodeBufferA(num6, num1, num2, shift_factor, predict_nr);
                         writer.Write((ushort)(num2 + 0.5));
 
-                        num1 = DecodeBlockB(num6, num2, num1, shift_factor, predict_nr);
+                        num1 = DecodeBufferB(num6, num2, num1, shift_factor, predict_nr);
                         writer.Write((ushort)(num1 + 0.5));
                     }
                 }
             }
             return outStream;
         }
-        private Stream Decode2Channel(Stream stream)
+        private Stream GetTwoChannelWavStream(Stream stream)
         {
             MemoryStream outStream = new MemoryStream();
             BinaryWriter writer = new BinaryWriter(outStream);
@@ -164,21 +144,21 @@ namespace OpenKh.Audio
                     int shift_factor2 = predict_nr2 & 15;
                     predict_nr2 >>= 4;
 
-                    for (int i = 2; i < 16; i++)
+                    for (int i = 2; i < 16; ++i)
                     {
                         int num6 = buffer[i];
                         int num7 = buffer[i + 16];
 
-                        num2 = DecodeBlockA(num6, num1, num2, shift_factor1, predict_nr1);
+                        num2 = DecodeBufferA(num6, num1, num2, shift_factor1, predict_nr1);
                         writer.Write((ushort)(num2 + 0.5));
 
-                        num4 = DecodeBlockA(num7, num3, num4, shift_factor2, predict_nr2);
+                        num4 = DecodeBufferA(num7, num3, num4, shift_factor2, predict_nr2);
                         writer.Write((ushort)(num4 + 0.5));
 
-                        num1 = DecodeBlockB(num6, num2, num1, shift_factor1, predict_nr1);
+                        num1 = DecodeBufferB(num6, num2, num1, shift_factor1, predict_nr1);
                         writer.Write((ushort)(num1 + 0.5));
 
-                        num3 = DecodeBlockB(num7, num4, num3, shift_factor2, predict_nr2);
+                        num3 = DecodeBufferB(num7, num4, num3, shift_factor2, predict_nr2);
                         writer.Write((ushort)(num3 + 0.5));
                     }
                 }
@@ -186,5 +166,22 @@ namespace OpenKh.Audio
             return outStream;
         }
 
+        private double DecodeBufferA(int i, double prevAdd, double inout, int shift_factor, int predict_nr)
+        {
+            int d = (i & 15) << 12;
+            if ((d & 32768) != 0)
+                d |= -65536;
+            inout = (d >> shift_factor) + prevAdd * f[predict_nr, 0] + inout * f[predict_nr, 1];
+            return inout;
+        }
+
+        private double DecodeBufferB(int i, double prevAdd, double inout, int shift_factor, int predict_nr)
+        {
+            int d = (i & 240) << 8;
+            if ((d & 32768) != 0)
+                d |= -65536;
+            inout = (d >> shift_factor) + prevAdd * f[predict_nr, 0] + inout * f[predict_nr, 1];
+            return inout;
+        }
     }
 }
