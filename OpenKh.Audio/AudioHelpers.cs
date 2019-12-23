911df9db7b4c434a318af0038e767c79780f66f3
Rikux3 <d.drankov@gmail.com>
2019-12-23 16:47:40 +0100

    Moved WAV header creation to helper class

---
+++ OpenKh.Audio/AudioHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace OpenKh.Audio
+{
+    public static class AudioHelpers
+    {
+        public static Stream BuildWavHeader(int sampleCount, int channels, int sampleRate)
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(ms);
+            writer.Write(0x46464952);                   //RIFF
+            writer.Write(50 + (channels * 2) * sampleCount);
+            writer.Write(0x45564157);                   //WAVE
+            writer.Write(0x20746D66);                   //fmt
+            writer.Write(18);
+            writer.Write((short)1);
+            writer.Write((short)channels);
+            writer.Write(sampleRate);
+            writer.Write(sampleRate * (channels * 2));
+            writer.Write((short)(channels * 2));
+            writer.Write((short)16);
+            writer.Write((short)0);
+            writer.Write(0x74636166);                   //fact
+            writer.Write(4);
+            writer.Write(sampleCount);
+            writer.Write(0x61746164);                   //data
+            writer.Write((channels * 2) * sampleCount);
+            return ms;
+        }
+    }
+}
