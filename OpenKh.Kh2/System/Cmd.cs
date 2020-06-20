3ddc38f22592e678f145ab1c2295ca078f9bf472
Rikux3 <d.drankov@gmail.com>
2020-06-20 15:17:05 +0200

    wip commit

---
+++ OpenKh.Kh2/System/Cmd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xe.BinaryMapper;
+
+namespace OpenKh.Kh2.System
+{
+    public class Cmd
+    {
+        [Data] public ushort Id { get; set; }
+        [Data] public ushort Unk02 { get; set; }
+        [Data] public short Unk04 { get; set; }
+        [Data] public byte SubMenuId { get; set; }
+        [Data] public byte IconId { get; set; }
+        [Data] public int TextId { get; set; }
+        [Data] public int Unk0C { get; set; }
+        //[Data] public ushort Unk10 { get; set; }
+        [Data(Count = 0x20)] public byte[] Unk10 { get; set; }
+    }
+}
---
+++ OpenKh.Tests/kh2/CmdTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using OpenKh.Common;
+using OpenKh.Kh2.System;
+using OpenKh.Kh2;
+using System.Linq;
+
+namespace OpenKh.Tests.kh2
+{
+    public class CmdTests
+    {
+        [Fact]
+        public void Check() => Common.FileOpenRead(@"kh2/res/cmd.bin", x => x.Using(stream =>
+        {
+            var table = BaseTable<Cmd>.Read(stream);
+            var grouped = table.Items.GroupBy(x => x.Unk0C).ToList();
+            Assert.Equal(0x2EF, table.Count);
+        }));
+    }
+}
